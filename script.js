de1c06498fc7e45b8f2348306b8c5fba1617b0a4
ianmcdavid <ianmcdavid25@gmail.com>
2020-09-23 15:24:44 -0700

    Reformatted for mobile only

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 $("#answer").hide();
 var magic8Ball = {};
-magic8Ball.listOfAnswers = ["Shake me harder...", "No. Nice try fool", "Yesmas Claus", "Maybe... ", "Either way, ur getting worked", "I've got 5 on it", "You better believe it!", "Does Darrel barrel? Of course!", "Probably not, nitz", "My sources tell me ur a nitz, so..", "All signs point to yes... Not!", "Aww hellll NO", "Absofrigginlutely", "In your dreams sucka", "You wish lol", "Yes, but only if you pay me"];
+magic8Ball.listOfAnswers = ["Shake me harder...", "No. Nice try fool", "Yesmas Claus", "Maybe... ", "Either way, ur getting worked", "I've got 5 on it", "You better believe it!", "Does Darrel barrel? Of course!", "Probably not, nitz", "My sources tell me ur a nitz, so..", "All signs point to yes... Not!", "Aww hellll NO", "Absofriggin lutely", "In your dreams sucka", "You wish lol", "Yes, but only if you pay me"];
 magic8Ball.tellFuture = function(question)
 {
 
