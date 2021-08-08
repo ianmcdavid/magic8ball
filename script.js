a7a08f3d8731221d92842c790624290109b0ccaa
ianmcdavid <ianmcdavid25@gmail.com>
2021-08-07 19:35:40 -0700

    Reformatted for desktop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 $("#answer").hide();
 var magic8Ball = {};
-magic8Ball.listOfAnswers = ["Shake me harder...", "No. Nice try fool", "Yesmas Claus", "Maybe... ", "Either way, ur getting worked", "I've got 5 on it", "You better believe it!", "Does Darrel barrel? Of course!", "Probably not, nitz", "My sources tell me ur a nitz, so..", "All signs point to yes... Not!", "Aww hellll NO", "Absofriggin lutely", "In your dreams sucka", "You wish lol", "Yes, but only if you pay me"];
+magic8Ball.listOfAnswers = ["Shake me harder...", "No. Nice try.", "Yeeesss?", "Maybe... ", "Either way, it's gonna suck", "I've got 5 on it", "You better believe it!", "Of course!", "Probably not...", "My sources tell me they're not paid enough...", "All signs point to yes... Not!", "Aww hellll NO", "Absofriggin lutely", "In your dreams sucka", "You wish lol", "Yes, but only if you pay me", "Not in a million years"];
 magic8Ball.tellFuture = function(question)
 {
 
@@ -26,7 +26,7 @@ var onClick = function()
   $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
 setTimeout(
 		function(){
-  var question = prompt("Don't be a nitz... Yes or no questions only."); 
+  var question = prompt("You know how this works... Yes or no questions only."); 
   magic8Ball.tellFuture(question);
 	 }, 500);
   	
